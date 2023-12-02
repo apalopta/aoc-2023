b7995d43372296c4d5054b8d413d920407e93629
Anja Papatola <mamadev@apalopta.de>
2023-12-02 13:14:03 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,57 @@
 fun main() {
+    fun sumUp(input: List<String>): Int = input
+        .map { it.replace("[a-z]".toRegex(), "") }
+        .map {if (it.length == 0) "0" else "${it.first()}${it.last()}" }
+        .map { it.toInt() }
+        .sum()
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return sumUp(input)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return sumUp(input.replaceNumberStrings(replacements))
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(readInput("Day01-test-1")) == 142)
+    check(part2(readInput("Day01-test-2")) == 281)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01-input")
     part1(input).println()
     part2(input).println()
 }
+
+fun List<String>.replaceNumberStrings(replacements: Map<String, String>): List<String> {
+    return this.map {
+        it.replaceAllInString(replacements)
+    }
+}
+
+private fun String.replaceAllInString(replacements: Map<String, String>): String {
+    var string = this
+    val sb = StringBuilder()
+    while (string.length > 0) {
+        val k = replacements.keys.find { k -> string.startsWith(k) }
+        if (k != null) {
+            string = string.removePrefix(k)
+            sb.append(replacements[k])
+        } else {
+            sb.append(string[0])
+            string = string.drop(1)
+        }
+    }
+    return sb.toString()
+}
+
+val replacements = mapOf(
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9"
+)
