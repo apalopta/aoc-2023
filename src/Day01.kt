b793850d957275f9505ba773f99131262a90e451
Anja Papatola <mamadev@apalopta.de>
2023-12-02 20:34:20 +0100

    correct Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,11 @@
 fun main() {
-    fun sumUp(input: List<String>): Int = input
-        .map { it.replace("[a-z]".toRegex(), "") }
-        .map {if (it.length == 0) "0" else "${it.first()}${it.last()}" }
-        .map { it.toInt() }
-        .sum()
 
-    fun part1(input: List<String>): Int {
-        return sumUp(input)
-    }
+    fun part1(input: List<String>): Int = input.map { it.firstAndLastDigit() }.sumOf { it.toInt() }
 
-    fun part2(input: List<String>): Int {
-        return sumUp(input.replaceNumberStrings(replacements))
-    }
+    fun part2(input: List<String>): Int = input.map { it.firstAndLastNumber() }.sumOf { it.toInt() }
 
     // test if implementation meets criteria from the description, like:
-    check(part2(readInput("Day01-test-1")) == 142)
+    check(part1(readInput("Day01-test-1")) == 142)
     check(part2(readInput("Day01-test-2")) == 281)
 
     val input = readInput("Day01-input")
@@ -22,6 +13,30 @@ fun main() {
     part2(input).println()
 }
 
+fun String.firstAndLastDigit(): String = "${firstOrNull { it.isDigit() } ?: "0"}${lastOrNull { it.isDigit() } ?: "0"}"
+
+private fun String.firstAndLastNumber(): String = "${this.firstDigit(Direction.FORWARD)}${this.firstDigit(Direction.BACKWARD)}"
+
+private fun String.firstDigit(direction: Direction): String? {
+    val indices = if (direction == Direction.FORWARD) this.indices else this.indices.reversed()
+    for (i in indices) {
+        if (this[i].isDigit()) return "${this[i]}"
+        replacements.keys.firstOrNull() { k -> this.substring(i).startsWith(k) }?.let {
+            return replacements[it]
+        }
+    }
+    return "0"
+}
+
+enum class Direction {
+    FORWARD,
+    BACKWARD;
+}
+
+// =============================================================================
+// This does work with the test data, but is wrong:
+// "eighthree" in the end will be "8hree", but the correct result is produced only if we evaluate to "eigh3", i.e. from the end!
+// unfortunately this is not clearly stated in the description
 fun List<String>.replaceNumberStrings(replacements: Map<String, String>): List<String> {
     return this.map {
         it.replaceAllInString(replacements)
