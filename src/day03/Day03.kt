497aa123b9ed37d1204f162e3f9af125dbbf92e5
Anja Papatola <mamadev@apalopta.de>
2023-12-03 22:09:11 +0100

    day 02 performance

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -1,4 +1,5 @@
 import kotlin.math.abs
+import kotlin.time.measureTime
 
 typealias Field = Int
 typealias EnginePositions = CharArray
@@ -6,17 +7,18 @@ typealias EnginePositions = CharArray
 fun main() {
     fun part1(input: List<String>): Int {
         with(Engine.from(input)) {
-            return getNeighbourNumbers() { lists ->
-                lists.map { it.sum() }
-            }
-                .sum()
+            return getGearNumbers { numbers ->
+                numbers
+                    .map { it.sum() }
+            }.sum()
         }
     }
 
     fun part2(input: List<String>): Int {
         with(Engine.from(input)) {
-            return getNeighbourNumbers() { lists ->
-                lists.filter { it.size == 2 }
+            return getGearNumbers { numbers ->
+                numbers
+                    .filter { it.size == 2 }
                     .map { it[0] * it[1] }
             }.sum()
         }
@@ -27,11 +29,14 @@ fun main() {
     check(part2(readInput("day03/test-1")) == 467835)
 
     val input = readInput("day03/input")
-    part1(input).println()
-    part2(input).println()
+
+    val time1 = measureTime { part1(input).println() }
+    println(time1.inWholeMilliseconds)
+    val time2 = measureTime { part2(input).println() }
+    println(time2.inWholeMilliseconds)
 }
 
-fun Engine.getNeighbourNumbers(c: (List<List<Int>>) -> List<Int>): List<Int> {
+fun Engine.getGearNumbers(c: (List<List<Int>>) -> List<Int>): List<Int> {
     val rangesToNumbers = getNumberRangesToNumbers()
     return gears.flatMap { gear ->
         val res = engineParts.mapIndexed { index, part ->
@@ -46,17 +51,51 @@ fun Engine.getNeighbourNumbers(c: (List<List<Int>>) -> List<Int>): List<Int> {
     }
 }
 
-// slower than just iterating - but okay for the moment
 fun Engine.getNumberRangesToNumbers(): Map<IntRange, Int> {
+
+    return findNumbersInRows()
+
+    // return findNumbersInString()
+}
+
+// finding numbers while iterating over rows - it's faster
+private fun Engine.findNumbersInRows(): Map<IntRange, Int> {
+    val rows = data.toList().chunked(width)
+    val map = mutableMapOf<IntRange, Int>()
+
+    var currentIndex = 0
+    rows.forEachIndexed { rowIndex, row ->
+        val chars = mutableListOf<Char>()
+        var startAt = 0
+        for ((i, char) in row.withIndex()) {
+            if (char.isDigit()) {
+                if (chars.isEmpty()) startAt = currentIndex
+                chars.add(char)
+            } else if (!char.isDigit() || i == (width - 1)) {
+                if (chars.isNotEmpty()) {
+                    map[(startAt until currentIndex)] = chars.joinToString("").toInt()
+                    chars.removeAll { true }
+                    startAt = 0
+                }
+            }
+            currentIndex++
+        }
+    }
+
+    return map
+}
+
+// alternative that reduces a string
+private fun Engine.findNumbersInString(): Map<IntRange, Int> {
     val replaced = data.joinToString("").replace("""[\D+]""".toRegex(), ".")
-    val numbers = mutableMapOf<IntRange, Int>()
     var string = replaced.dropWhile { it == '.' }
     var currentIndex = replaced.length - string.length
 
+    val numbers = mutableMapOf<IntRange, Int>()
     while (string.isNotEmpty()) {
         val candidate = string.substringBefore(".")
 
-        numbers[(currentIndex until currentIndex + candidate.length)] = candidate.toInt()
+        numbers[(currentIndex until (currentIndex + candidate.length))] = candidate.toInt()
 
         string = string.removePrefix(candidate)
         currentIndex += candidate.length
@@ -64,11 +103,10 @@ fun Engine.getNumberRangesToNumbers(): Map<IntRange, Int> {
         currentIndex += (string.length - newString.length)
         string = newString
     }
-
     return numbers
 }
 
-class Engine(val data: CharArray, width: Int, height: Int) : LinearGridBoard(width, height) {
+class Engine(val data: CharArray, val width: Int, val height: Int) : LinearGridBoard(width, height) {
     val engineParts = EnginePositions(width * height) {
         if (data[it].isDigit()) '.' else (if (data[it] != '.') data[it] else '.')
     }
